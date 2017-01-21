c13d75579ba7cfca00167fd066f170899be9e186
Sávio Carlos Martins Costa <saviocmc@gmail.com>
2017-01-21 01:10:12 -0200

    The POST method is now responsible for parsing the received data (plain text) in the body of the request. In case of an parsing exception, the server will respond with a 400 code (Bad Request).

--- server.js
+++ server.js
@@ -10,27 +10,6 @@ const CLIENTS_ID	= util.swap( JSON.parse( fs.readFileSync ( './clients.id' ) ) )
 // This object will be used to store all the information posted by the trackers
 const TRACKERS = { };
 for ( var token in TRACKERS_ID ) { var id = TRACKERS_ID [token] ; TRACKERS [id] = { "id":id } ; }
- 
-var server = require( 'http' ).createServer( (request, response) =>
-{
-	log.info("New " + request.method + " to " + request.url);
-	var resource = request.url.split("/") [1];
-	try {
-		if ( !( RESTfulMethod = RESTful [ request.method ] [ resource ] ) )
-			throw "Ressource \"" + resource + "\" not found in the RESTful methods map";
-	} catch (error) {
-		response.writeHead( 400 );
-		response.end( "400 - Bad Request" );
-		log.warn("Bad Request: " + error);
-		return;
-	}
-	RESTfulMethod (request, response);
-});
-
-// Starting the server with the configuration specified on the file "server.conf"
-var serverConf = JSON.parse( fs.readFileSync( "./server.conf" ) );
-server.listen( serverConf.port, serverConf.hostname, () =>
-	log.info( "Server started. Listening to: \n" + JSON.stringify( server.address(), null, 4 ) ) );
 
 const RESTful = { "GET" : { } , "POST" : { } }; // This map stores the references for the functions that will really handle the http requests
 
@@ -52,8 +31,15 @@ RESTful ["POST"] ["tracker"] = ( request, response ) =>
 	var tracker_id = request.url.split("/")[2];
 	// Validate the request using the "auth" property of the headers
 	if ( TRACKERS_ID [request.headers.auth] && TRACKERS_ID [request.headers.auth] == tracker_id ) {
-		// It will accept post with the maximun body size of 1KB (1000 B)
-		util. extractJsonBody( request, 1000, ( jsonBody ) => { 
+		// It will accept post requests with the maximun body size of 1KB (1000 B)
+		util. getRequestBody( request, 1000, ( body ) => { 
+			try {
+				jsonBody = JSON.parse(body);
+			} catch( error ) {
+				response.writeHead( 400 );
+				response.end( "400 - Bad Request" );
+				return;
+			}
 			response.writeHead( 200, { "Content-Type" : "application/json" } );
 			//Merge the propreties of the incomming object with the local object and return it as the http response
 			response.end( JSON.stringify( Object.assign( TRACKERS [tracker_id] , jsonBody ), null, 4 ) );
@@ -63,3 +49,24 @@ RESTful ["POST"] ["tracker"] = ( request, response ) =>
 		response.end( "403 - Forbidden" );
 	}
 }
+ 
+var server = require( 'http' ).createServer( (request, response) =>
+{
+	log.info( request.method + " request to " + request.url );
+	var resource = request.url.split("/") [1];
+	try {
+		if ( !( RESTfulMethod = RESTful [ request.method ] [ resource ] ) )
+			throw "Ressource \"" + resource + "\" not found in the RESTful methods map";
+	} catch (error) {
+		response.writeHead( 400 );
+		response.end( "400 - Bad Request" );
+		log.warn("Bad Request: " + error);
+		return;
+	}
+	RESTfulMethod (request, response);
+});
+
+// Starting the server with the configuration specified on the file "server.conf"
+var serverConf = JSON.parse( fs.readFileSync( "./server.conf" ) );
+server.listen( serverConf.port, serverConf.hostname, () =>
+	log.info( "Server started. Listening to: \n" + JSON.stringify( server.address(), null, 4 ) ) );
