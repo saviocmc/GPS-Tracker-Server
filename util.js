d614989328b3e4438a67ba446860550e45c6feb0
Sávio Carlos Martins Costa <saviocmc@gmail.com>
2017-01-21 01:07:09 -0200

    The function that extracts the body of a request does not parse the data anymore. Instead, it now calls the callback function passing the data in a string. The callback is responsable for parsing or do whathever it want. Now, all functions uses lambda expression.

--- util.js
+++ util.js
@@ -1,35 +1,29 @@
 console = new require( "console" ).Console( require( "fs" ).createWriteStream( "./log" ) );
 
-log = (logType, message) => console.log( "\n[%s] %s - %s", logType, new Date().toLocaleString( "pt-BR" ), message );
+log = ( logType, msg ) => console.log( "\n[%s] %s - %s", logType, new Date().toLocaleString( "pt-BR" ), msg );
 
 module.exports.log =
 {
-	"info" : (msg) => log("INFO", msg ),
-	"warn" : (msg) => log("WARNING", msg ),
-	"error" : (msg) => log("ERROR", msg )
+	"info" : (msg) => log( "INFO", msg ),
+	"warn" : (msg) => log( "WARNING", msg ),
+	"error" : (msg) => log( "ERROR", msg )
 }
 
-// This function waits the end of the connection and parses the body of the request in the Json format
-// Then, it calls the callback function with the Json object as the parameter.
-module.exports.extractJsonBody = function(request, maxRequestSize, callback){
-	var stringBody = "";
-	request.on('data', function (data) {
-		stringBody += data;
-		// kill the connection if the request is greater than the "maxRequestSize" parameter
-		if (stringBody.length > maxRequestSize)
-			request.connection.destroy();
-		});
-	request.on('end', function () {
-		var jsonBody = JSON.parse(stringBody);
-		callback(jsonBody);
-	});
+// This function puts a listener on the event of incoming data of the request and concatenate the data into a string object
+// When all the incoming data is read and concatenate into the string, this function calls the callback function with the string object as the parameter
+module.exports.getRequestBody = ( request, maxBodySize, callback ) =>
+{
+	var body = "";
+	request.on( 'data', ( data ) => ( body.length + data.length < maxBodySize ) ? body += data : request.connection.destroy() );
+	request.on( 'end', () => callback( body ) );
 }
 
-// Swaps the keys with the values of a Json object
-module.exports.swap = function(json){
+// Swaps the keys with the values of a object
+module.exports.swap = ( obj ) =>
+{
 	var swaped = { };
-	for(var key in json){
-		swaped [ json [ key ] ] = key;
+	for( var key in obj ) {
+		swaped [ obj [ key ] ] = key;
 	}
 	return swaped;
 }
